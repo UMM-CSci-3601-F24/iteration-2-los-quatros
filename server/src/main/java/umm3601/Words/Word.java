6896cad7a9a0c60923412c0c30eadc52f1c8cfe5
thegreenn1nja <73561889+thegreenn1nja@users.noreply.github.com>
2024-10-06 22:39:39 -0500

    Backend Server things
    Made WordController.java, Word.java, and attempted testing, however was unable to figure out what was going wrong due to not having wifi.

---
+++ server/src/main/java/umm3601/Words/Word.java
@@ -0,0 +1,35 @@
+package umm3601.Words;
+
+import org.mongojack.Id;
+import org.mongojack.ObjectId;
+
+@SuppressWarnings({"VisibilityModifier"})
+public class Word {
+  @ObjectId @Id
+
+  @SuppressWarnings({"MemberWord"})
+  public String _id;
+
+  public String word;
+  public String wordGroup;
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Word)) {
+      return false;
+    }
+    Word other = (Word) obj;
+    return _id.equals(other._id);
+  }
+
+  @Override
+  public int hashCode() {
+    return _id.hashCode();
+  }
+
+  @Override
+  public String toString() {
+    return word;
+  }
+}
+
---
+++ server/src/main/java/umm3601/Words/WordController.java
@@ -0,0 +1,153 @@
+package umm3601.Words;
+
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.regex;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+import org.bson.Document;
+import org.bson.UuidRepresentation;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+import org.mongojack.JacksonMongoCollection;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Sorts;
+import com.mongodb.client.result.DeleteResult;
+
+import io.javalin.Javalin;
+import io.javalin.http.BadRequestResponse;
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+import io.javalin.http.NotFoundResponse;
+import umm3601.Controller;
+
+public class WordController implements Controller {
+
+  private static final String API_WORD = "/api/word";
+  private static final String API_WORD_BY_ID = "/api/word/{id}";
+  // private static final String WORD_KEY = "word";
+  // private static final String WORDGROUP_KEY = "wordgroup";
+
+  private final JacksonMongoCollection<Word> wordCollection;
+
+  public WordController(MongoDatabase database) {
+    wordCollection = JacksonMongoCollection.builder().build(
+        database,
+        "users",
+        Word.class,
+        UuidRepresentation.STANDARD);
+  }
+
+  public void getWord(Context ctx) {
+    String id = ctx.pathParam("id");
+    Word words;
+
+    try {
+      words = wordCollection.find(eq("_id", new ObjectId(id))).first();
+    } catch (IllegalArgumentException e) {
+      throw new BadRequestResponse("The requested user id wasn't a legal Mongo Object ID.");
+    }
+    if (words == null) {
+      throw new NotFoundResponse("The requested user was not found");
+    } else {
+      ctx.json(words);
+      ctx.status(HttpStatus.OK);
+    }
+  }
+
+  public void getWords(Context ctx) {
+    Bson combinedFilter = constructFilter(ctx);
+
+    Bson sortingOrder = constructSortingOrder(ctx);
+
+    ArrayList<Word> matchingWords = wordCollection
+        .find(combinedFilter)   // Use the combined filter here
+        .sort(sortingOrder)     // Sort according to query parameters
+        .into(new ArrayList<>());
+
+    ctx.json(matchingWords);
+    ctx.status(HttpStatus.OK);
+}
+
+  private static final String WORD_KEY = "word";
+private static final String WORD_GROUP_KEY = "wordGroup";
+
+private Bson constructFilter(Context ctx) {
+    List<Bson> filters = new ArrayList<>();
+
+    // Filter by 'word' (case-insensitive regex match)
+    if (ctx.queryParamMap().containsKey(WORD_KEY)) {
+        Pattern wordPattern = Pattern.compile(Pattern.quote(ctx.queryParam(WORD_KEY)), Pattern.CASE_INSENSITIVE);
+        filters.add(regex(WORD_KEY, wordPattern));
+    }
+
+    // Filter by 'wordGroup' (case-insensitive regex match)
+    if (ctx.queryParamMap().containsKey(WORD_GROUP_KEY)) {
+        Pattern wordGroupPattern = Pattern.compile(Pattern.quote(ctx.queryParam(WORD_GROUP_KEY)), Pattern.CASE_INSENSITIVE);
+        filters.add(regex(WORD_GROUP_KEY, wordGroupPattern));
+    }
+
+    Bson combinedFilter = filters.isEmpty() ? new Document() : and(filters);
+
+    return combinedFilter;
+}
+
+
+
+  private Bson constructSortingOrder(Context ctx) {
+    String sortBy = Objects.requireNonNullElse(ctx.queryParam("sortby"), "word");
+    String sortOrder = Objects.requireNonNullElse(ctx.queryParam("sortorder"), "asc");
+    Bson sortingOrder = sortOrder.equals("desc") ?  Sorts.descending(sortBy) : Sorts.ascending(sortBy);
+    return sortingOrder;
+  }
+
+  public void addNewWord(Context ctx) {
+    String body = ctx.body();
+    Word newWord = ctx.bodyValidator(Word.class)
+        .check(wrd -> wrd.word != null && wrd.word.length() > 0,
+            "Word must be non-empty; body was " + body)
+        .check(wrd -> wrd.wordGroup != null && wrd.wordGroup.length() > 0,
+            "Word Group must be non-empty; body was " + body)
+        .get();
+
+    wordCollection.insertOne(newWord);
+    ctx.json(Map.of("id", newWord._id));
+    ctx.status(HttpStatus.CREATED);
+  }
+
+  public void deleteWord(Context ctx) {
+    String id = ctx.pathParam("id");
+    DeleteResult deleteResult = wordCollection.deleteOne(eq("_id", new ObjectId(id)));
+    // We should have deleted 1 or 0 users, depending on whether `id` is a valid
+    // user ID.
+    if (deleteResult.getDeletedCount() != 1) {
+      ctx.status(HttpStatus.NOT_FOUND);
+      throw new NotFoundResponse(
+          "Was unable to delete ID "
+              + id
+              + "; perhaps illegal ID or an ID for an item not in the system?");
+    }
+    ctx.status(HttpStatus.OK);
+  }
+
+  public void addRoutes(Javalin server) {
+    // Get the specified word
+    server.get(API_WORD_BY_ID, this::getWord);
+
+    // List words, filtered using query parameters
+    server.get(API_WORD, this::getWords);
+
+    // Delete the specified word
+    server.delete(API_WORD_BY_ID, this::deleteWord);
+
+    // Add new word with the word info being in the JSON body
+    // of the HTTP request
+    server.post(API_WORD, this::addNewWord);
+  }
+}
