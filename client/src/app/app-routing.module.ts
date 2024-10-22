5726d3598869863266eeaa8c385bde18520ebf6b
Keenan McCall <keenanmccall@outlook.com>
2024-10-22 14:57:08 -0500

    Deleted User Files
    Deleted all user files so that we can get an accurate reading of our code coverage overall.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AddUserComponent } from './users/add-user.component';
-import { UserListComponent } from './users/user-list.component';
+
+
 import { WordListComponent } from './anagram/word-list.component';
-import { UserProfileComponent } from './users/user-profile.component';
-import { CompanyListComponent } from './company-list/company-list.component';
+
+
 import { AddWordComponent } from './anagram/add-word.component';
 
 // Note that the 'users/new' route needs to come before 'users/:id'.
@@ -13,15 +13,13 @@ import { AddWordComponent } from './anagram/add-word.component';
 // 'users/new'; the router would just think that the string 'new' is a user ID.
 const routes: Routes = [
   {path: '', component: HomeComponent, title: 'Home'},
-  {path: 'users', component: UserListComponent, title: 'Users'},
   {path: 'anagram', component: WordListComponent, title: 'Anagram'},
   {path: 'anagram/new', component: AddWordComponent, title: 'Add Word'},
   // this is kind of backwards but allows us to pass an id for deleteWord without having profile
   {path: 'anagram/:id', component: WordListComponent, title: 'Anagram'},
   {path: 'anagram/:wordGroup', component: WordListComponent, title: 'Anagram'},
-  {path: 'users/new', component: AddUserComponent, title: 'Add User'},
-  {path: 'users/:id', component: UserProfileComponent, title: 'User Profile'},
-  {path: 'companies', component: CompanyListComponent, title: 'Companies'}
+
+
 ];
 
 @NgModule({
