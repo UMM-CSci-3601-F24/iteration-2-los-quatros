bdb2a0ddcbdbfc83aa94f6b6a8fe868e8a7661fb
thegreenn1nja <73561889+thegreenn1nja@users.noreply.github.com>
2024-11-03 19:00:02 -0600

    Revert "Added more testing for wordlistspec and addwordspec"
    This reverts commit 551f098297b9403e6908f462164154bdeb2bbbdd.

--- client/src/app/anagram/word-list.component.ts
+++ client/src/app/anagram/word-list.component.ts
@@ -1,174 +1,133 @@
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-// import { MatCardModule } from '@angular/material/card';
-
-
+import { Component, computed, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormField } from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { WordService } from './word.service';
-import { MockWordService } from 'src/testing/word.service.mock';
-import { Word } from './word';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { of, throwError, Observable } from 'rxjs';
-import { HttpErrorResponse } from '@angular/common/http';
-import { RouterTestingModule } from '@angular/router/testing';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { catchError, combineLatest, of, switchMap, tap } from 'rxjs';
+import { Word } from './word';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltip } from '@angular/material/tooltip';
+import { MatNavList } from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { Router } from '@angular/router';
 
-const COMMON_IMPORTS: unknown[] = [
-  RouterTestingModule,
-  BrowserAnimationsModule,
-]
 
-describe('Word List', () => {
-  let wordList: WordListComponent;
-  let fixture: ComponentFixture<WordListComponent>;
-  let wordService: WordService;
-  let snackBar: MatSnackBar;
+@Component({
+  selector: 'app-word-list-component',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatFormField,
+    MatRadioModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatIconModule,
+    RouterLink,
+    MatButtonModule,
+    MatTooltip,
+    MatNavList,
+    MatListModule,
+    MatInputModule,
+    MatSlideToggleModule,
+  ],
+  templateUrl: './word-list.component.html',
+  styleUrl: './word-list.component.scss'
+})
+export class WordListComponent {
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [COMMON_IMPORTS, WordListComponent],
-      providers: [{provide: WordService, useValue: new MockWordService() }, MatSnackBar],
-    });
-  });
 
-  beforeEach(waitForAsync(() => {
-    TestBed.compileComponents().then(() => {
-      fixture = TestBed.createComponent(WordListComponent);
-      wordList = fixture.componentInstance;
-      wordService = TestBed.inject(WordService);
-      snackBar = TestBed.inject(MatSnackBar);
-      fixture.detectChanges();
-    });
-  }));
+  // client side sorting
+  sortType = signal<string | undefined>(undefined);
+  sortOrder = signal<boolean | undefined>(false);
+  //server side filtering
+  contains = signal<string|undefined>(undefined);
+  group = signal<string|undefined>(undefined);
 
-  it('should create', () => {
-    expect(wordList).toBeTruthy();
-  });
 
-  it('contains all the words', () => {
-    expect(wordList.serverFilteredWords().length).toBe(5);
-  });
+  errMsg = signal<string | undefined>(undefined);
 
-  it("contains a word 'Mac'", () => {
-    expect(
-      wordList.serverFilteredWords().some((word: Word) => word.word === 'Mac')
-    ).toBe(true);
-  });
 
-  it('has four words in the group `team members`', () => {
-    expect(
-      wordList.serverFilteredWords().filter((word: Word) => word.wordGroup === "team member").length
-    ).toBe(4);
-  });
 
-  it('has one word in the group `teachers`', () => {
-    expect(
-      wordList.serverFilteredWords().filter((word: Word) => word.wordGroup === "teachers").length
-    ).toBe(1);
-  });
 
-  it('should call deleteWord and show a Snackbar message', () => {
-    spyOn(wordService, 'deleteWord').and.returnValue(of(undefined));
-    spyOn(snackBar, 'open');
 
-    wordList.deleteWord('testId');
 
-    expect(wordService.deleteWord).toHaveBeenCalledWith('testId');
-    expect(snackBar.open).toHaveBeenCalledWith('We deleted a word!', 'OK', { duration: 6000 });
-  });
+  constructor(private wordService: WordService, private snackBar: MatSnackBar, private router: Router) {
+  }
 
-  it('should handle HttpErrorResponse and show an error Snackbar', () => {
-    const errorResponse = new HttpErrorResponse({
-      error: 'test 404 error',
-      status: 404,
-      statusText: 'Not Found'
-    });
 
-    spyOn(wordService, 'getWords').and.returnValue(throwError(() => errorResponse));
-    spyOn(snackBar, 'open');
+  private contains$ = toObservable(this.contains);
+  private group$ = toObservable(this.group);
 
-    wordList.ngOnInit();
 
-    expect(wordList.errMsg()).toContain('Problem contacting the server – Error Code: 404\nMessage: test 404 error');
-    expect(snackBar.open).toHaveBeenCalledWith(
-      'Problem contacting the server – Error Code: 404\nMessage: test 404 error',
-      'OK',
-      { duration: 6000 }
-    );
-  });
-
-  it('should handle client-side error and show an error Snackbar', () => {
-    const errorEvent = new ErrorEvent('Network error', {
-      message: 'simulated network error'
-    });
-
-    const errorResponse = new HttpErrorResponse({
-      error: errorEvent,
-      status: 0,
-      statusText: 'Unknown Error'
-    });
-
-    spyOn(wordService, 'getWords').and.returnValue(throwError(() => errorResponse));
-    spyOn(snackBar, 'open');
-
-    wordList.ngOnInit();
-
-    expect(wordList.errMsg()).toContain('Problem in the client – Error: simulated network error');
-    expect(snackBar.open).toHaveBeenCalledWith(
-      'Problem in the client – Error: simulated network error',
-      'OK',
-      { duration: 6000 }
-    );
-  });
-});
-
-describe('misbehaving word list', () => {
-  let wordList: WordListComponent;
-  let fixture: ComponentFixture<WordListComponent>;
-  let originalTimeout;
-
-  let wordServiceStub: {
-    getWords: () => Observable<Word[]>;
-  };
-
-  beforeEach(() => {
-    wordServiceStub = {
-      getWords: () =>
-        new Observable((observer) => {
-          observer.error('getWords() Observer generates and error');
+  serverFilteredWords =
+    toSignal(
+      combineLatest([this.contains$, this.group$]).pipe(
+        switchMap(([word, wordGroup]) =>
+          this.wordService.getWords({
+            word,
+            wordGroup,
+          })
+        ),
+        catchError((err) => {
+          if (err.error instanceof ErrorEvent) {
+            this.errMsg.set(
+              `Problem in the client – Error: ${err.error.message}`
+            );
+          } else {
+            this.errMsg.set(
+              `Problem contacting the server – Error Code: ${err.status}\nMessage: ${err.message}`
+            );
+          }
+          this.snackBar.open(this.errMsg(), 'OK', { duration: 6000 });
+          return of<Word[]>([]);
         }),
-    };
-    TestBed.configureTestingModule({
-      imports: [WordListComponent, COMMON_IMPORTS],
-      providers: [{provide: WordService, useValue: wordServiceStub}],
+        tap(() => {
+
+
+        })
+      )
+    );
+
+
+  filteredWords = computed(() => {
+    const serverFilteredWords = this.serverFilteredWords();
+    return this.wordService.sortWords(serverFilteredWords, {
+      sortType: this.sortType(),
+      sortOrder: this.sortOrder(),
     });
   });
 
-  beforeEach(waitForAsync(() => {
-    TestBed.compileComponents().then(() => {
-      fixture = TestBed.createComponent(WordListComponent);
-      wordList = fixture.componentInstance;
-      fixture.detectChanges();
-    });
-  }));
 
-  // these two functions are a workaround to build more time
-  //into this test so it does not auto fail when another test fails
-  beforeEach(function() {
-      originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-      jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-  });
-  afterEach(function() {
-      jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
-  });
+  deleteWord(id: string) {
+    // const tempSortType = this.sortType.toString;
+    this.wordService.deleteWord(id).subscribe(() => {
+      // this is to refresh the page eventually
+      // this.sortType.set(undefined);
+      // this.sortType.set(tempSortType.toString());
+      this.snackBar.open(`We deleted a word!`, 'OK', {duration: 6000});
+    })
+  }
 
-  it('generates an error if we don`t set up a WordListService', () => {
-    jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-    expect(wordList.serverFilteredWords())
-      .withContext('service cant give values of non-existent list')
-      .toEqual([]);
-    expect(wordList.errMsg())
-      .withContext('the error message will be')
-      .toContain('Problem contacting the server – Error Code:');
-  });
-});
-export { WordListComponent };
 
+  deleteWordGroup(group: string) {
+    this.wordService.deleteWordGroup(group).subscribe(() => {
+      this.snackBar.open(`We deleted a word group!`, 'OK', {duration: 6000});
+    })
+    // if(group.length >= 1) {
+
+
+    // } else {
+    //   this.snackBar.open('Failed to delete word group', 'OK', {duration: 6000});
+    // }
+  }
+}
